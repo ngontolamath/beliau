0a40886fa3846428188ee33eb10b06bbc6789991
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-02 14:29:58 +0700

    cuttly.js

--- plugins/cuttly.js
+++ plugins/cuttly.js
@@ -1,13 +1,13 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
   if (!text) throw 'url/link nya mana?'
-  let res = await fetch(`http://hadi-api.herokuapp.com/api/cuttly?url=${text}`)
+  let res = await fetch(`https://saipulanuar.ga/api/short/tinyurl?url=${text}`)
   let json = await res.json()
   if (json.status) m.reply(json.result)
   else throw 'Link Invalid!\nPeriksa url anda'
 }
-handler.help = ['cuttly'].map(v => v + ' <link>')
+handler.help = ['tinyurl'].map(v => v + ' <link>')
 handler.tags = ['shortlink']
-handler.command = /^cuttly$/i
+handler.command = /^tinyurl$/i
 
 module.exports = handler
