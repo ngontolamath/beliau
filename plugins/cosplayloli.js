b3316260b4a907e7433c73efdb5de6ba10e889af
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-02 13:44:39 +0700

    cosplayloli.js

--- plugins/cosplayloli.js
+++ plugins/cosplayloli.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
      m.reply('tunggu...')
   let res = await (await fetch('https://raw.githubusercontent.com/Luigmntng/RESTAPI/master/data/cosplayloli.json')).json()
   let cosser = res[Math.floor(Math.random() * res.length)]
-  await conn.sendButtonImg(m.chat, cosser, 'istri gw cosplay 😳', 'By Lui', 'Next', `.cosplayloli`, m, false)
+  await conn.sendFile(m.chat, cosser, 'Cosplay.jpg', 'Jangan dijilat bang', m, false)
 }
 handler.help = ['cosplayloli']
 handler.tags = ['anime']
