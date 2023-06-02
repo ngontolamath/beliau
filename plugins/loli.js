b885801bfeb59c6b24fbb4d6fd7908e181c5a913
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-02 15:13:50 +0700

    loli.js

--- plugins/loli.js
+++ plugins/loli.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
      m.reply('tunggu...')
   let res = await (await fetch('https://raw.githubusercontent.com/Luigmntng/RESTAPI/master/data/loli.json')).json()
   let anjime = res[Math.floor(Math.random() * res.length)]
-  await conn.sendButtonImg(m.chat, anjime, 'Pedo kartun bego', wm, 'Next', `.loli`, m, false)
+  await conn.sendFile(m.chat, anjime, 'loli.jpg', 'pedo kartun', m)
 }
 handler.help = ['loli']
 handler.tags = ['anime']
