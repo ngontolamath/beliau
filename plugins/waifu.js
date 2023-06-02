c4c3cf6e09f7154f1748dcf1437cdd78e500fb6a
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-02 14:58:07 +0700

    waifu.js

--- plugins/waifu.js
+++ plugins/waifu.js
@@ -4,12 +4,12 @@ let handler = async (m, { conn }) => {
   if (!res.ok) throw 'Error Website sedang down'
   let json = await res.json()
   if (!json.url) throw 'Error!'
-  conn.sendButtonImg(m.chat, json.url, 'Wibu ngentod', 'tolol', 'Next', `.waifu`, m, 0, { thumbnail: Buffer.alloc(0) })
+  conn.sendFile(m.chat, json.url, 'wibu.jpg', 'Wibu ngentod', m)
 }
 handler.help = ['waifu']
 handler.tags = ['anime']
 handler.command = /^(waifu)$/i
 
-handler.limit = true
+handler.limit = false
 
 module.exports = handler
