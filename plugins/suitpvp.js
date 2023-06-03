61516103948156d930177b6bcece34a402b1a2b0
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-03 19:00:19 +0700

    Update suitpvp.js

--- plugins/suitpvp.js
+++ plugins/suitpvp.js
@@ -16,11 +16,10 @@ _*SUIT PvP*_
 
 @${m.sender.split`@`[0]} menantang @${m.mentionedJid[0].split`@`[0]} untuk bermain suit
 
-Silahkan @${m.mentionedJid[0].split`@`[0]} 
+Silahkan @${m.mentionedJid[0].split\n\nKetik "terima/ok/gas" untuk memulai suit\nKetik "tolak/gabisa/nanti" untuk menolak`@`[0]} 
 `.trim()
-  let footer = `Ketik "terima/ok/gas" untuk memulai suit\nKetik "tolak/gabisa/nanti" untuk menolak`
   conn.suit[id] = {
-    chat: await conn.send2Button(m.chat, caption, footer, 'Terima', 'ok', 'Tolak', 'tolak', m, { contextInfo: { mentionedJid: conn.parseMention(caption) } }),
+    chat: await conn.reply(m.chat, caption, m, { contextInfo: { mentionedJid: conn.parseMention(caption) } }),
     id: id,
     p: m.sender,
     p2: m.mentionedJid[0],
