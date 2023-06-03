29d6f860f50c53a99450b311497ec14f547dbc4f
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-03 14:19:03 +0700

    Update hent.js

--- plugins/hent.js
+++ plugins/hent.js
@@ -1,14 +1,14 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn, usedPrefix, command }) => {
-     m.reply('tunggu...')
-  let res = await fetch(`https://hmtai.herokuapp.com/nsfw/${command}`)
+let handler = async (m, { conn, command }) => {
+     m.reply('Ksabar')
+  let res = await fetch(`https://raw.githubusercontent.com/NzrlAfndi/Databasee/main/nsfw/${command}.json}`)
   let json = await res.json()
   if (!json.url) throw json
   await conn.sendButtonImg(m.chat, json.url, 'sagne sama kartun telanjang?', `random ${command}`, 'Next', `${usedPrefix + command}`, m, false)
 }
-handler.help = ['ass', 'bdsm', 'blowjob', 'boobjob', 'cum', 'creampie', 'cuckold', 'ero', 'elves', 'femdom', 'foot', 'gangbang', 'glasses', 'hentai', 'incest', 'masturbation', 'pantsu', 'orgy', 'tentacles', 'thighs', 'uniform', 'vagina', 'yuri' ]
+handler.help = ['ahegao', 'ass', 'bdsm', 'blowjob', 'boobjob', 'cum', 'creampie', 'cuckold', 'ero', 'elves', 'femdom', 'foot', 'gangbang', 'glasses', 'hentai', 'incest', 'masturbation', 'pantsu', 'orgy', 'tentacles', 'thighs', 'uniform', 'vagina', 'yuri' ]
 handler.tags = ['dewasa']
-handler.command = /^(ass|bdsm|blowjob|boobjob|cum|creampie|cuckold|ero|elves|femdom|foot|gangbang|glasses|hentai|incest|masturbation|pantsu|orgy|tentacles|thighs|uniform|vagina|yuri)$/i
+handler.command = /^(ahegao|ass|bdsm|blowjob|boobjob|cum|creampie|cuckold|ero|elves|femdom|foot|gangbang|glasses|hentai|incest|masturbation|pantsu|orgy|tentacles|thighs|uniform|vagina|yuri)$/i
 
 handler.private = false
 handler.limit = 10
