e46aca101d36b1aae47e4e4c3be7a141f8199ab7
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-03 14:54:03 +0700

    Update hent.js

--- plugins/hent.js
+++ plugins/hent.js
@@ -1,19 +1,24 @@
-let fetch = require('node-fetch')
+const fetch = require('node-fetch')
+
 let handler = async (m, { conn, command }) => {
-     m.reply('Ksabar')
-  let res = await fetch(`https://raw.githubusercontent.com/NzrlAfndi/Databasee/main/nsfw/${command}.json}`)
-  let data = await res.json()
-  let json = data[Math.floor(Math.random() * data.length)]
+  m.reply('Ksabar')
+  try {
+    let res = await fetch(`https://raw.githubusercontent.com/NzrlAfndi/Databasee/main/nsfw/${command}.json`)
+    let data = await res.json()
+    let json = data[Math.floor(Math.random() * data.length)]
   
-  
-await conn.sendFile(m.chat, json, 'error.jpg', `tolol`, m)
+    await conn.sendFile(m.chat, json, 'error.jpg', 'tolol', m)
+  } catch (error) {
+    console.error(error)
+    m.reply('Terjadi kesalahan')
+  }
 }
-handler.help = ['ahegao', 'ass', 'bdsm', 'blowjob', 'boobjob', 'cum', 'creampie', 'cuckold', 'ero', 'elves', 'femdom', 'foot', 'gangbang', 'glasses', 'hentai', 'incest', 'masturbation', 'pantsu', 'orgy', 'tentacles', 'thighs', 'uniform', 'vagina', 'yuri' ]
+
+handler.help = ['ahegao', 'ass', 'bdsm', 'blowjob', 'boobjob', 'cum', 'creampie', 'cuckold', 'ero', 'elves', 'femdom', 'foot', 'gangbang', 'glasses', 'hentai', 'incest', 'masturbation', 'pantsu', 'orgy', 'tentacles', 'thighs', 'uniform', 'vagina', 'yuri']
 handler.tags = ['dewasa']
 handler.command = /^(ahegao|ass|bdsm|blowjob|boobjob|cum|creampie|cuckold|ero|elves|femdom|foot|gangbang|glasses|hentai|incest|masturbation|pantsu|orgy|tentacles|thighs|uniform|vagina|yuri)$/i
-
 handler.private = false
-handler.limit = 10
+handler.premium = true
 handler.register = false
 
 module.exports = handler
