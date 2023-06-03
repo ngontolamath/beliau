d68ed1936a993d9a17b6fa641087573d6dfb53de
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-03 17:33:08 +0700

    Delete elf.js

--- plugins/elf.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('lolhuman', '/api/random/elf', {}, 'apikey'), 'Nih', wm, 'NEXT', '.elf', m)
-}
-handler.help = ['elf']
-handler.tags = ['anime']
-handler.command = /^(elf)$/i
-
-module.exports = handler
