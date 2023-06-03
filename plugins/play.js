02d7ba56cc0fd4a39ce46040e0377b7877a75897
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-03 17:19:14 +0700

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -25,13 +25,12 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
   if (yt === false) throw 'semua server gagal'
   if (yt2 === false) throw 'semua server gagal'
   let { dl_link, thumb, title, filesize, filesizeF } = yt
-  await conn.sendFile(m.chat, await (await fetch(thumb)).buffer(), 'thumbnail
-.jpg', `
-*Judul:* ${title}
+  await conn.sendFile(m.chat, await (await fetch(thumb)).buffer(), 'thumb.jpg',
+`*Judul:* ${title}
 *Ukuran File Audio:* ${filesizeF}
 *Ukuran File Video:* ${yt2.filesizeF}
 *Server y2mate:* ${usedServer}
-`.trim())
+`.trim(), m)
 }
 handler.help = ['play'].map(v => v + ' <pencarian>')
 handler.tags = ['downloader']
