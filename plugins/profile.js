5c4af116d056a240b5ec57d7e4fdb38edd203474
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-04 01:59:23 +0700

    Update profile.js

--- plugins/profile.js
+++ plugins/profile.js
@@ -1,34 +1,38 @@
-let fetch = require('node-fetch')
-let PhoneNumber = require('awesome-phonenumber')
+const fetch = require('node-fetch');
+const PhoneNumber = require('awesome-phonenumber');
+
 let handler = async (m, { conn }) => {
-  let pp = './src/avatar_contact.png'
-  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let pp = './src/avatar_contact.png';
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+  
   try {
-    pp = await conn.profilePictureUrl(who, 'image')
+    pp = await conn.getProfilePicture(who);
   } catch (e) {
-
+    // Jika terjadi error, akan tetap menggunakan avatar default
   } finally {
-    let { name, premium, level, limit, exp, lastclaim, registered, regTime, age } = global.DATABASE.data.users[m.sender]
-    let username = conn.getName(who)
+    let { name, premium, level, limit, exp, lastclaim, registered, regTime, age } = global.DATABASE.data.users[m.sender];
+    let username = conn.getName(who);
     let str = `
-
 ✧───────[ *PROFILE* ]───────✧
 📇 • *Name:* ${username} ${registered ? '(' + name + ') ': ''}
 📧 • *Tag:* @${who.replace(/@.+/, '')}
 📞 • *Number:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
 💻 • *Link:* https://wa.me/${who.split`@`[0]}
 ${registered ? '🎨 • *Age:* ' + age : ''}
-
 🌟 • *Premium:* ${premium ? "✅" :"❌"}
 📑 • *Registered:* ${registered ? '✅': '❌'}
 ⛔ • *Banned:* ❌
+`.trim();
 
-`.trim()
-    let mentionedJid = [who]
-    conn.sendFile(m.chat, await(await require('node-fetch')(pp)).buffer(), pp.jpg, str, m, false, { contextInfo: { mentionedJid }})
+    let mentionedJid = [who];
+    let response = await fetch(pp);
+    let buffer = await response.buffer();
+    conn.sendFile(m.chat, buffer, 'pp.jpg', str, m, false, { contextInfo: { mentionedJid } });
   }
-}
-handler.help = ['profile [@user]']
-handler.tags = ['tools']
-handler.command = /^profile|pp$/i
+};
+
+handler.help = ['profile [@user]'];
+handler.tags = ['tools'];
+handler.command = /^profile|pp$/i;
+
 module.exports = handler
