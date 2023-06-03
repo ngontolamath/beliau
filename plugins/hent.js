f068ab2cd4c53bfdad1f9c057a70a88a64f810e3
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-03 14:25:57 +0700

    Update hent.js

--- plugins/hent.js
+++ plugins/hent.js
@@ -2,9 +2,11 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, command }) => {
      m.reply('Ksabar')
   let res = await fetch(`https://raw.githubusercontent.com/NzrlAfndi/Databasee/main/nsfw/${command}.json}`)
-  let json = await res.json()
-  if (!json.url) throw json
-  await conn.sendButtonImg(m.chat, json.url, 'sagne sama kartun telanjang?', `random ${command}`, 'Next', `${usedPrefix + command}`, m, false)
+  let data = await res.json()
+  let json = data[Math.floor(Math.random() * data.length)]
+  
+  
+await conn.sendFile(m.chat, json, 'error.jpg', `tolol`, m)
 }
 handler.help = ['ahegao', 'ass', 'bdsm', 'blowjob', 'boobjob', 'cum', 'creampie', 'cuckold', 'ero', 'elves', 'femdom', 'foot', 'gangbang', 'glasses', 'hentai', 'incest', 'masturbation', 'pantsu', 'orgy', 'tentacles', 'thighs', 'uniform', 'vagina', 'yuri' ]
 handler.tags = ['dewasa']
@@ -15,5 +17,3 @@ handler.limit = 10
 handler.register = false
 
 module.exports = handler
-
-//by lui
