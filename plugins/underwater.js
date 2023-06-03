b52ee8bf722649ce6db5ac506e86b580d0c146fb
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-03 19:15:13 +0700

    Update underwater.js

--- plugins/underwater.js
+++ plugins/underwater.js
@@ -1,10 +1,17 @@
 let fetch = require('node-fetch')
-let handler = async (m, {conn, text}) => {
-   m.reply('Ksabar')
-  let res = `https://saipulanuar.ga/api/textpro/drop-water?text=${text}`
-  conn.sendFile(m.chat, res, 'underwatee.jpg', `Sudah jadi`, m)
+
+let handler = async (m, { conn, text }) => {
+  try {
+    m.reply('Ksabar')
+    let res = `https://saipulanuar.ga/api/textpro/drop-water?text=${encodeURIComponent(text)}`
+    conn.sendFile(m.chat, res, 'underwater.jpg', 'Sudah jadi', m)
+  } catch (error) {
+    console.error(error)
+    m.reply('Terjadi kesalahan')
+  }
 }
-handler.help = ['underwater'].map(v => v + ' <text>')
+
+handler.help = ['underwater'].map((v) => v + ' <text>')
 handler.tags = ['maker']
 handler.command = /^(underwater)$/i
 
