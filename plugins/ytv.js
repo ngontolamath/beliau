d946982d4ef7514d74e5d40d8fa5fd4b348f49de
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-04 14:01:32 +0700

    Update ytv.js

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -1,27 +1,22 @@
-let limit = 1024
-let fetch = require('node-fetch')
-const { servers, ytv } = require('../lib/y2mate')
-let handler = async (m, { conn, args, isPrems, isOwner }) => {
-  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
-  let chat = global.db.data.chats[m.chat]
-  let server = (args[1] || servers[0]).toLowerCase()
-  let { dl_link, thumb, title, filesize, filesizeF} = await ytv(args[0], servers.includes(server) ? server : servers[0])
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
-`.trim(), m)
-  let _thumb = {}
-  try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
-  catch (e) { }
-  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp4', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-`.trim(), m, false, {
-  ..._thumb,
-  asDocument: chat.useDocument
-})
+const axios = require("axios");
+let handler = async (m, { conn, text }) => {
+   if (!text) {
+   return conn.reply(m.chat, 'URL tidak ditemukan', m);
+  }
+ try {
+    let response = await axios.get(`https://saipulanuar.ga/api/download/ytmp4?url=${text}`);
+    let result = response.data.result;
+    let message = `
+*Judul*: ${result.title}
+*Channel*: ${result.channel}
+*Upload*: ${result.published}
+`;
+  conn.sendFile(m.chat, result.url, 'error.mp4', message, m);
+  } catch (error) {
+
+    console.log(error);
+    conn.reply(m.chat, 'Terjadi kesalahan', m)
+  }
 }
 handler.help = ['mp4','v',''].map(v => 'yt' + v + ` <url>`)
 handler.tags = ['downloader']
@@ -40,21 +35,3 @@ handler.exp = 0
 handler.limit = true
 
 module.exports = handler
-
-/*let xfar = require('xfarr-api')
-let fetch = require('node-fetch')
-let handler = async (m, { conn, command, text }) => {
-    if (!text) throw 'Masukkan Link\n\nContoh: .ytmp4 https://youtube.com/xxxxxx'
-  let res = await xfar.Youtube(text)
-m.reply('*Tunggu Sebentar...*')
-conn.sendFile(m.chat,res.medias[2].url, 'audio.mp3', `Youtube Downloader
-360p`, m)
-
-}
-handler.help = ['ytmp4 <url>']
-handler.tags = ['downloader']
-handler.command = /^ytmp4$/i
-
-
-module.exports = handler*/
-
