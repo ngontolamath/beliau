8bb22384df378e0c01f93e694e13e97b5ba4360b
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-04 14:24:34 +0700

    Update cecan.js

--- plugins/cecan.js
+++ plugins/cecan.js
@@ -1,8 +1,15 @@
-let handler  = async (m, { conn }) => {
-     conn.sendFile(m.chat, 'https://saipulanuar.ga/api/cecan/${command}', 'foto.jpg', 'ahhhh', m)
+let handler = async (m, { conn }) => {
+  try {
+    conn.sendFile(m.chat, `https://saipulanuar.ga/api/cecan/${m.command}`, 'foto.jpg', 'ahhhh', m)
+  } catch (error) {
+    console.log(error)
+    conn.reply(m.chat, 'Terjadi kesalahan', m)
+  }
 }
-handler.help = ['random', 'hijaber', 'indonesia', 'malaysia', 'vietnam', 'thailand(error)', 'japan', 'korea', 'china']
-handler.tag = ['cecan']
+
+handler.help = ['random', 'hijaber', 'indonesia', 'malaysia', 'vietnam', 'thailand', 'japan', 'korea', 'china']
+handler.tags = ['cecan']
 handler.command = /^(random|hijaber|indonesia|malaysia|vietnam|thailand|japan|korea|china)$/i
+handler.limit = true
 
 module.exports = handler
