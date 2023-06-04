23abd4f887224262d46abd2ec2eb001cab5a2b31
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-04 14:44:39 +0700

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -2,10 +2,10 @@ let fetch = require('node-fetch')
 let fs = require('fs')
 let handler = async(m, { conn, usedPrefix, text, command }) => {
     if (!text) throw `Harap masukkan URL\n\nContoh : .fb https://www.facebook.com/alanwalkermusic/videos/277641643524720`
-    let res = await fetch(`https://api-kaysa.herokuapp.com/api/fbdl?url=${text}&apikey=KaysaS`)
-    if (!res.ok) throw await `${res.status} ${res.statusText}`
+    let res = await fetch(`https://saipulanuar.ga/api/download/fb?url=${text}`)
+    if (!res.ok) throw await `error`
     let json = await res.json()
-    await conn.sendFile(m.chat, json.medias.url, 'kntl.mp4', `*Nih*`, m)
+    await conn.sendFile(m.chat, json.result.hd, 'kntl.mp4', `*${global.wm}*`, m)
 }
 handler.help = ['fb'].map(v => v + ' <url>')
 handler.tags = ['downloader']
