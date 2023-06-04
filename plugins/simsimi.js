59769ceffa57c7a1095f2c0404327875401ca44e
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-05 00:17:34 +0700

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,13 +1,14 @@
-let fetch = require('node-fetch')
-let handler = async (m, { text }) => {
-  let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "id" }, ''))
-  let json = await res.json()
-  if (json.success) m.reply(json.success)
-  else throw json
+let axios = require("axios");
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+  if (!text) return conn.reply(m.chat, 'Masukkan teks/nContoh: .${command} halo dek',  m)
+	axios.get(`https://saipulanuar.ga/api/f/simi?text=${text}`).then ((res) => {	
+    let hasil = `${res.data.result}`
+    conn.reply(m.chat, hasil, m)
+	})
 }
-handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
+handler.help = ['simi <teks>', 'simsimi <teks>', 'ai <teks>']
 handler.tags = ['fun']
-handler.command = /^((sim)?simi|simih)$/i
+handler.command = /^(simi|simsimi|ai)$/i
 
 module.exports = handler
 
