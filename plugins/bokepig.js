369dd09c33375d1d144ca684227c37be47654cda
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-04 14:09:12 +0700

    Update bokepig.js

--- plugins/bokepig.js
+++ plugins/bokepig.js
@@ -1,10 +1,6 @@
-async function getRandom(url) {
-    return Math.floor(Math.random() * url)
-    }
-    
-     let handler  = async (m, { conn }) => {
+let handler  = async (m, { conn }) => {
          conn.sendFile(m.chat, `https://saipulanuar.ga/api/bokepig`, 'bokepig.jpg', `cuma gambar`, m)
-    }
+ }
 handler.help = ['bokepig (bkp image)']
 handler.tags = ['dewasa', 'premium']
 handler.command = /^(bokepig)$/i
