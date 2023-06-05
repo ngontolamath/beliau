9950af42b5cca47749156dd997280cb924c4f3a5
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-05 13:53:38 +0700

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,16 +1,23 @@
-const fetch = require('node-fetch');
-let handler = async(m, { conn, text, usedPrefix, command }) => {
-  if (!text) return conn.reply(m.chat, 'Masukkan teks/nContoh: .${command} halo dek',  m)
-	let res = await fetch('https://saipulanuar.ga/api/f/simi?text=${text}')
-   if (!res.ok) throw eror
+let fetch = require('node-fetch')
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  try {
+    if (!text) {
+      throw `Masukkan teks\n\nContoh: ${usedPrefix}${command} halo dek`
+    }
+    let res = await fetch(`https://saipulanuar.ga/api/f/simi?text=${text}`)
+    if (!res.ok) {
+      throw `Terjadi kesalahan`
+    }
     let data = await res.json()
     let hasil = '${data.result}'
-conn.reply(m.chat, hasil, m)
-	
+    conn.reply(m.chat, hasil, m)
+  } catch (error) {
+    console.error(error)
+    await conn.reply(m.chat, 'error', m)
+  }
 }
 handler.help = ['simi <teks>', 'simsimi <teks>', 'ai <teks>']
 handler.tags = ['fun']
 handler.command = /^(simi|simsimi|ai)$/i
 
 module.exports = handler
-
