a343cb2722f60c98a36ece924ab2a5321fa753eb
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-06 00:08:21 +0700

    Update cecan.js

--- plugins/cecan.js
+++ plugins/cecan.js
@@ -1,15 +1,21 @@
-let handler = async (m, {conn, command}) => {
+const fetch = require('node-fetch')
+
+let handler = async (m, { conn, command }) => {
+  m.reply('Ksabar')
   try {
-    conn.sendFile(m.chat, `https://saipulanuar.ga/api/cecan/${command}`, 'foto.jpg', 'ahhhh', m)
+    let res = await fetch(`https://raw.githubusercontent.com/ngontolamath/databasee/main/cecan/${command}.json`)
+    let data = await res.json()
+    let json = data[Math.floor(Math.random() * data.length)]
+  
+    await conn.sendFile(m.chat, json, 'error.jpg', 'tolol', m)
   } catch (error) {
-    console.log(error)
-    conn.reply(m.chat, 'Terjadi kesalahan', m)
+    console.error(error)
+    m.reply('Terjadi kesalahan')
   }
 }
-
-handler.help = ['random', 'hijaber', 'indonesia', 'malaysia', 'vietnam', 'thailand', 'japan', 'korea', 'china']
+handler.help = ['indonesia', 'malaysia', 'vietnam', 'thailand', 'japan', 'korea', 'china']
 handler.tags = ['cecan']
-handler.command = /^(random|hijaber|indonesia|malaysia|vietnam|thailand|japan|korea|china)$/i
+handler.command = /^(indonesia|malaysia|vietnam|thailand|japan|korea|china)$/i
 handler.limit = true
 
 module.exports = handler
