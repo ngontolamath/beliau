e5b6beb790392c8dcefd480b9e226a3ec32a9be6
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-05 14:20:26 +0700

    Update susunkata_ans.js

--- plugins/susunkata_ans.js
+++ plugins/susunkata_ans.js
@@ -1,28 +1,22 @@
 const similarity = require('similarity')
-const threshold = 0.72
-
-let handler = async (m) => {
-  let id = m.chat
-  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*suka/i.test(m.quoted.contentText)) return true
-  this.susunkata = this.susunkata ? this.susunkata : {}
-  if (!(id in this.susunkata)) return m.reply('Soal itu telah berakhir')
-  if (m.quoted.id == this.susunkata[id][0].id) {
-    let json = JSON.parse(JSON.stringify(this.susunkata[id][1]))
-    if (['.suka', 'Bantuan', ''].includes(m.text)) return true
-    if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
-      if (!global.db.data.users[m.sender]) global.db.data.users[m.sender] = {}
-      if (!global.db.data.users[m.sender].exp) global.db.data.users[m.sender].exp = 0
-      global.db.data.users[m.sender].exp += this.susunkata[id][2]
-      await conn.reply(m.chat, `*Benar!* +${this.susunkata[id][2]} XP`)
-      clearTimeout(this.susunkata[id][3])
-      delete this.susunkata[id]
-    } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) {
-      conn.reply(m.chat, `*Dikit Lagi!*`)
-    } else {
-      conn.reply(m.chat, `*Salah!*`)
+let handler = m => m
+handler.before = async function (m) {
+    let id = m.chat
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*who/i.test(m.quoted.text)) return !0
+    this.susunkata = this.susunkata ? this.susunkata : {}
+    if (!(id in this.susunkata)) return m.reply('Soal itu telah berakhir')
+    if (m.quoted.id == this.susunkata[id][0].id) {
+        let json = JSON.parse(JSON.stringify(this.susunkata[id][1]))
+        if (['.who', 'Bantuan', ''].includes(m.text)) return !0
+        if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
+            global.db.data.users[m.sender].exp += this.susunkata[id][2]
+            this.reply(m.chat, `*Benar!* +${this.susunkata[id][2]} XP`, m)
+                     clearTimeout(this.susunkata[id][3])
+            delete this.susunkata[id]
+        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= 0.72) m.reply(`*Dikit Lagi!*`)
+        else m.reply(`*Salah!*`)
     }
-  }
-  return true
+    return !0
 }
 handler.exp = 0
 module.exports = handler
