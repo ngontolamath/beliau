1a6c6af797511905e5c0c6a1001acbb60378ef6b
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-05 13:45:57 +0700

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,13 +1,12 @@
 const fetch = require('node-fetch');
 let handler = async(m, { conn, text, usedPrefix, command }) => {
   if (!text) return conn.reply(m.chat, 'Masukkan teks/nContoh: .${command} halo dek',  m)
+	let res = await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/asahotak.json');
+   if (!res.ok) throw eror
+    let data = await res.json()
+    let hasil = '${data.url}'
+conn.reply(m.chat, hasil, m)
 	
-
-	
-
-	
-    conn.reply(m.chat, hasil, m)
-	})
 }
 handler.help = ['simi <teks>', 'simsimi <teks>', 'ai <teks>']
 handler.tags = ['fun']
