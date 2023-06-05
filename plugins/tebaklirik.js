5e168f2495e8d8a728723d073f4df749ae2fc77b
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-05 23:32:46 +0700

    Update tebaklirik.js

--- plugins/tebaklirik.js
+++ plugins/tebaklirik.js
@@ -22,7 +22,7 @@ Ketik ${usedPrefix}teli untuk bantuan
 Bonus: ${poin} XP
     `.trim()
     conn.tebaklirik[id] = [
-        await conn.sendBut(m.chat, caption, wm, 'Bantuan', '.teli', m),
+        await conn.reply(m.chat, caption, m),
         json, poin,
         setTimeout(() => {
             if (conn.tebaklirik[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, conn.tebaklirik[id][0])
