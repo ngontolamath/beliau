96a461aef801628232c22668c509b666b7ed0569
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-05 15:46:21 +0700

    Update husbu.js

--- plugins/husbu.js
+++ plugins/husbu.js
@@ -1,7 +1,10 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn, usedPrefix, command }) => {
+let handler = async (m, { conn }) => {
 m.reply(KsabarL)
-  let res = await fetch
+  let res = await fetch('https://raw.githubusercontent.com/NzrlAfndi/Databasee/main/anime/husbu.json')
+  let data = await res.json()
+  let json = data[Math.floor(Math.random() * data.length)]
+     conn.sendFile(m.chat, json, 'eror.jpg', 'gantengan owner', m)
 }
 handler.help = ['husbu']
 handler.tags = ['anime']
