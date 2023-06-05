dc4e2df26510d35440e446dbcea40da21797afa1
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-05 14:25:24 +0700

    Update tebakgambar_ans.js

--- plugins/tebakgambar_ans.js
+++ plugins/tebakgambar_ans.js
@@ -2,12 +2,12 @@ const similarity = require('similarity')
 let handler = m => m
 handler.before = async function (m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*who/i.test(m.quoted.text)) return !0
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*hint/i.test(m.quoted.text)) return !0
     this.tebakgambar = this.tebakgambar ? this.tebakgambar : {}
     if (!(id in this.tebakgambar)) return m.reply('Soal itu telah berakhir')
     if (m.quoted.id == this.tebakgambar[id][0].id) {
         let json = JSON.parse(JSON.stringify(this.tebakgambar[id][1]))
-        if (['.who', 'Bantuan', ''].includes(m.text)) return !0
+        if (['.hint', 'Bantuan', ''].includes(m.text)) return !0
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.tebakgambar[id][2]
             this.reply(m.chat, `*Benar!* +${this.tebakgambar[id][2]} XP`, m)
