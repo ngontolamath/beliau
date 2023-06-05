60b22e144c3cf0ca5fdfcc33c30e07ff6d6c7cf2
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-05 15:56:10 +0700

    Update apakah.js

--- plugins/apakah.js
+++ plugins/apakah.js
@@ -1,15 +1,22 @@
 let handler = async (m, { conn, text }) => {
-  conn.reply(m.chat, `${pickRandom(['Yap','Sepertinya Begitu','Kayaknya','Kayaknya nggak','Nggak','Nggak mungkin'])}
+  conn.reply(m.chat, `${pickRandom(['Yap','Sepertinya Begitu','Kayaknya','Kayaknya nggak','Nggak','Nggak mungkin'])}`
 `.trim(), m, m.mentionedJid ? {
   contextInfo: {
     mentionedJid: m.mentionedJid
   }
 } : {})
 }
-handler.help = ['apakah <teks>?', 'apa <teks>?]
+handler.help = ['', 'kah'].map(v => 'apa' + v + ' <text>?')
 handler.tags = ['kerang']
-handler.command = /^(apakah|apa)$/i
+handler.command = /^apa(kah)?$/i
 handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
 
 handler.fail = null
 
