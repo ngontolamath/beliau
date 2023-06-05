00232f0d1a2aa893c2d1bcb70c87af170185e144
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-05 15:19:17 +0700

    Update tebakkabupaten_ans.js

--- plugins/tebakkabupaten_ans.js
+++ plugins/tebakkabupaten_ans.js
@@ -2,12 +2,12 @@ const similarity = require('similarity')
 let handler = m => m
 handler.before = async function (m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*who/i.test(m.quoted.text)) return !0
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*tebu/i.test(m.quoted.text)) return !0
     this.tebakkabupaten = this.tebakkabupaten ? this.tebakkabupaten : {}
     if (!(id in this.tebakkabupaten)) return m.reply('Soal itu telah berakhir')
     if (m.quoted.id == this.tebakkabupaten[id][0].id) {
         let json = JSON.parse(JSON.stringify(this.tebakkabupaten[id][1]))
-        if (['.who', 'Bantuan', ''].includes(m.text)) return !0
+        if (['.tebu', 'Bantuan', ''].includes(m.text)) return !0
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.tebakkabupaten[id][2]
             this.reply(m.chat, `*Benar!* +${this.tebakkabupaten[id][2]} XP`, m)
