d7547133a7eb1795d989801f1505750694f99590
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-05 14:17:42 +0700

    Update asahotak_ans.js

--- plugins/asahotak_ans.js
+++ plugins/asahotak_ans.js
@@ -1,30 +1,22 @@
-const similarity = require('similarity');
-const threshold = 0.72;
-
-let handler = async function (m) {
-    let id = m.chat;
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*ao/i.test(m.quoted.contentText)) return true;
-    
-    this.asahotak = this.asahotak ? this.asahotak : {};
-    if (!(id in this.asahotak)) return m.reply('Soal itu telah berakhir');
-    
+const similarity = require('similarity')
+let handler = m => m
+handler.before = async function (m) {
+    let id = m.chat
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*who/i.test(m.quoted.text)) return !0
+    this.asahotak = this.asahotak ? this.asahotak : {}
+    if (!(id in this.asahotak)) return m.reply('Soal itu telah berakhir')
     if (m.quoted.id == this.asahotak[id][0].id) {
-        let json = JSON.parse(JSON.stringify(this.asahotak[id][1]));
-        if (['.ao', 'Bantuan', ''].includes(m.text)) return true;
-        
+        let json = JSON.parse(JSON.stringify(this.asahotak[id][1]))
+        if (['.who', 'Bantuan', ''].includes(m.text)) return !0
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
-            global.db.data.users[m.sender].exp += this.asahotak[id][2];
-            await this.reply(m.chat, `*Benar!* +${this.asahotak[id][2]} XP`);
-            clearTimeout(this.asahotak[id][3]);
-            delete this.asahotak[id];
-        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) {
-            m.reply(`*Dikit Lagi!*`);
-        } else {
-            m.reply(`*Salah!*`);
-        }
+            global.db.data.users[m.sender].exp += this.asahotak[id][2]
+            this.reply(m.chat, `*Benar!* +${this.asahotak[id][2]} XP`, m)
+                     clearTimeout(this.asahotak[id][3])
+            delete this.asahotak[id]
+        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= 0.72) m.reply(`*Dikit Lagi!*`)
+        else m.reply(`*Salah!*`)
     }
-    
-    return true;
-};
-
-module.exports = handler;
+    return !0
+}
+handler.exp = 0
+module.exports = handler
