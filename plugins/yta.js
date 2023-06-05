3373b0e969b8a876363e3a22396740cf22d94ea0
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-05 16:05:31 +0700

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -1,43 +1,22 @@
-const axios = require("axios");
-
+const axios = require("axios")
 let handler = async (m, { conn, text }) => {
-
   if (!text) {
-
-    return conn.reply(m.chat, 'URL tidak ditemukan', m);
-
+    return conn.reply(m.chat, 'URL tidak ditemukan', m)
   }
-
   try {
-
-    const response = await axios.get(`https://saipulanuar.ga/api/download/ytmp3?url=${text}`);
-
-    const result = response.data.result;
-
-    const message = `
-
-*Judul*: ${result.title}
-
-*Channel*: ${result.channel}
-
-*Upload*: ${result.published}
-
-    `;
-
-    conn.sendFile(m.chat, result.url,  0,0,m,true, {ptt:true })
-
+    let response = await axios.get(`https://saipulanuar.ga/api/download/ytmp3?url=${text}`)
+    let result = response.data.result
+conn.sendFile(m.chat, result, null, null, m, /vn/.test(args[0]), { quoted: m, mimetype: 'audio/mp4' })
   } catch (error) {
-    console.log(error);
-    conn.reply(m.chat, 'Terjadi kesalahan saat mengunduh audio', m);
-
+    console.log(error)
+    conn.reply(m.chat, 'Terjadi kesalahan saat mengunduh audio', m)
   }
 }
-handler.help = ['mp3', 'a'].map(v => 'yt' + v + ' <url>');
-handler.tags = ['downloader'];
+handler.help = ['mp3', 'a'].map(v => 'yt' + v + ' <url>')
+handler.tags = ['downloader']
 handler.command = /^yt(a|mp3)$/i;
-handler.fail = null;
-handler.exp = 0;
-handler.limit = true;
+handler.fail = null
+handler.exp = 0
+handler.limit = true
 
-module.exports = handler;
-
+module.exports = handler
