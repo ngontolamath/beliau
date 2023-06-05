b00520cdb22b00e90955e9a947db59f84eede476
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-05 15:11:52 +0700

    Update asahotak_ans.js

--- plugins/asahotak_ans.js
+++ plugins/asahotak_ans.js
@@ -2,12 +2,12 @@ const similarity = require('similarity')
 let handler = m => m
 handler.before = async function (m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*who/i.test(m.quoted.text)) return !0
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*ao/i.test(m.quoted.text)) return !0
     this.asahotak = this.asahotak ? this.asahotak : {}
     if (!(id in this.asahotak)) return m.reply('Soal itu telah berakhir')
     if (m.quoted.id == this.asahotak[id][0].id) {
         let json = JSON.parse(JSON.stringify(this.asahotak[id][1]))
-        if (['.who', 'Bantuan', ''].includes(m.text)) return !0
+        if (['.ao', 'Bantuan', ''].includes(m.text)) return !0
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.asahotak[id][2]
             this.reply(m.chat, `*Benar!* +${this.asahotak[id][2]} XP`, m)
