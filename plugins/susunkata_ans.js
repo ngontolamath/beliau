faae3e5c2efad23776254a07e81a9cb64e312db0
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-05 14:27:00 +0700

    Update susunkata_ans.js

--- plugins/susunkata_ans.js
+++ plugins/susunkata_ans.js
@@ -2,12 +2,12 @@ const similarity = require('similarity')
 let handler = m => m
 handler.before = async function (m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*who/i.test(m.quoted.text)) return !0
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*suka/i.test(m.quoted.text)) return !0
     this.susunkata = this.susunkata ? this.susunkata : {}
     if (!(id in this.susunkata)) return m.reply('Soal itu telah berakhir')
     if (m.quoted.id == this.susunkata[id][0].id) {
         let json = JSON.parse(JSON.stringify(this.susunkata[id][1]))
-        if (['.who', 'Bantuan', ''].includes(m.text)) return !0
+        if (['.suka', 'Bantuan', ''].includes(m.text)) return !0
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.susunkata[id][2]
             this.reply(m.chat, `*Benar!* +${this.susunkata[id][2]} XP`, m)
