0c7281d870c5cf9a5cdbdb122fed0e3962c9d01e
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-05 16:42:05 +0700

    Update tebakanjime.js

--- plugins/tebakanjime.js
+++ plugins/tebakanjime.js
@@ -1,32 +1,32 @@
-let fetch = require('node-fetch')
-
+const fetch = require('node-fetch')
 let timeout = 120000
 let poin = 500
-let handler = async (m, { conn, usedPrefix, command }) => {
-    conn.tebakanime = conn.tebakanime ? conn.tebakanime : {}
-    let id = m.chat
-    if (id in conn.tebakanime) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakanime[id][0])
-        throw false
-    }
-    let res = await (await fetch('https://raw.githubusercontent.com/Luigmntng/RESTAPI/master/data/tebakchara.json)).json()
-    let json = res[Math.floor(Math.random() * res.length)]
-    let caption = `
-Timeout *${(timeout / 1000).toFixed(2)} detik*
+let handler = async (m, { conn, usedPrefix }) => {
+  conn.tebakanime = conn.tebakanime ? conn.tebakanime : {}
+  let id = m.chat
+  if (id in conn.tebakanime) {
+    conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakanime[id][0])
+    throw false
+  }
+  let src = await (await fetch('https://raw.githubusercontent.com/NzrlAfndi/Databasee/main/games/tebakchara.json')).json()
+  let json = src[Math.floor(Math.random() * src.length)]
+  let caption = `
+  Timeout *${(timeout / 1000).toFixed(2)} detik*
 Ketik ${usedPrefix}wa untuk bantuan
 Bonus: ${poin} XP
-`.trim()
-    conn.tebakanime[id] = [
-        await conn.sendFile(m.chat, json.image, 'eror.jpg', caption, m),
-        json, poin,
-        setTimeout(async () => {
-            if (conn.tebakanime[id]) await conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.name}\nDesk : ${json.desc}*`, conn.tebakanime[id][0])
-            delete conn.tebakanime[id]
-        }, timeout)
-    ]
+    `.trim()
+  conn.tebakanime[id] = [
+    await conn.sendFile(m.chat, json.image, 'animek.jpg', caption, m)
+    ,
+    json, poin,
+    setTimeout(async () => {
+      if (conn.tebakanime[id]) await conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.name}*`, conn.tebakanime[id][0])
+      delete conn.tebakanime[id]
+    }, timeout)
+  ]
 }
 handler.help = ['tebakanime']
 handler.tags = ['game']
-handler.command = /^tebakanime$/i
+handler.command = /^tebakanime/i
 
 module.exports = handler
