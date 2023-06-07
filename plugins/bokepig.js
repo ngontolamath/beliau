dfeafd309335bceeb417382f239ebc20a0b6734b
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-07 17:42:01 +0700

    Update bokepig.js

--- plugins/bokepig.js
+++ plugins/bokepig.js
@@ -1,5 +1,5 @@
 let handler  = async (m, { conn }) => {
-         conn.sendFile(m.chat, `https://saipulanuar.ga/api/bokepig`, 'bokepig.jpg', `cuma gambar`, m)
+         conn.sendFile(m.chat, `https://saipulanuar.cf/api/bokepig`, 'bokepig.jpg', `cuma gambar`, m)
  }
 handler.help = ['bokepig (bkp image)']
 handler.tags = ['dewasa', 'premium']
