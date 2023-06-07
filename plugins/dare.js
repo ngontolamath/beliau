d40d06d461601592da926d0f3908d94c2145ed7c
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-07 19:00:22 +0700

    Update dare.js

--- plugins/dare.js
+++ plugins/dare.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let handler  = async (m) => {
   let res = await fetch('https://raw.githubusercontent.com/BochilGaming/database/master/kata-kata/dare.json')
   let json = await res.json()
-  conn.sendBut(m.chat, `${pickRandom(json)}`, `ini wm`, 'Dare', `.dare`, m)
+  conn.reply(m.chat, `${pickRandom(json)}`, `ini wm`, 'Dare' m)
 }
 handler.help = ['dare']
 handler.tags = ['quotes']
