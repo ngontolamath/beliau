2fb988e6092d4eb070b712ae74ffa03ee2de70ed
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-07 18:50:00 +0700

    Update kopi.js

--- plugins/kopi.js
+++ plugins/kopi.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, args }) => {
   );
   if (!res.ok) throw await `${res.status} ${res.statusText}`;
   let json = await res.json();
-  if (json.file) conn.sendButtonImg(m.chat, json.file, "kopi", wm, 'Lagi', '.kopi', m);
+  if (json.file) conn.sendFile(m.chat, json.file, 'error.jpg', 'kopi😋', m);
   else throw json;
 };
 handler.help = ["coffee", "kopi"];
