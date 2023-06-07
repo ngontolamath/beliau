63da9b3dc38b309b98c91f0a327d3d8db282bc5b
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-07 19:04:20 +0700

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -3,14 +3,9 @@ let handler = async (m, { conn, text }) => {
   if (!text) {
     return conn.reply(m.chat, 'URL tidak ditemukan', m)
   }
-  try {
     let res = await fetch(`https://saipulanuar.cf/api/download/ytmp3?url=${text}`)
     let json = await res.json()
 conn.sendFile(m.chat, json.result.url, 'awkwokeror.mp3', '', m)
-  } catch (error) {
-    console.log(error)
-    conn.reply(m.chat, 'Terjadi kesalahan saat mengunduh audio', m)
-  }
 }
 handler.help = ['mp3', 'a'].map(v => 'yt' + v + ' <url>')
 handler.tags = ['downloader']
