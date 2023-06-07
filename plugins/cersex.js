2b87b0cf90ccf659c607fece4a07fcf51a520a53
ngontolamath <108114391+ngontolamath@users.noreply.github.com>
2023-06-07 18:39:36 +0700

    Update cersex.js

--- plugins/cersex.js
+++ plugins/cersex.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, text }) => {
     //if (!text) return conn.reply(m.chat, 'Masukan parameter, Contoh *#lirik my love*', m)
 
   await m.reply('KsabarL')
-	axios.get(`https://saipulanuar.ga/api/cerpen/random`).then ((res) => {
+	axios.get(`https://saipulanuar.cf/api/cerpen/random`).then ((res) => {
 	 	let hasil = `${res.data.result}`
 
     conn.reply(m.chat, hasil, m)
